369e97decb60d11c34d86ef9632a81977cdca253
Icetrack <63660650+ICL32@users.noreply.github.com>
2023-05-10 21:05:27 +0300

    Hide network behaviour from component menu

--- .idea/.idea.NULE-T.dir/.idea/sonarlint/issuestore/index.pb
+++
@@ -1,5 +0,0 @@
-
-=
-manifest.json,c\a\ca642a22686971b261b60de81d8ff554fdd91e2e
-<
-package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
--- Runtime/Nule/Nule/src/Nule/NetworkBehaviour.cs
+++ Runtime/Nule/Nule/src/Nule/NetworkBehaviour.cs
@@ -2,13 +2,12 @@ using UnityEngine;
 
 namespace Nule
 {
+    [HideInInspector]
     public class NetworkBehaviour : MonoBehaviour
     {
         public bool isServer { get; internal set; }
         public bool isLocalPlayer { get; internal set; }
         private static ulong _identifiers;
         public ulong InstanceId { get; } = _identifiers++;
-
-        
     }
 }
